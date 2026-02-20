c5e445bdcfdf10f2b99e2ab26624f98ebd705e85
Juliusouko <julius.ouko@student.moringaschool.com>
2026-02-20 08:21:01 +0300

    send test email, email delivered to test account inbox

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use dotenvy::dotenv;
+use resend_rs::types::CreateEmailBaseOptions;
+use resend_rs::{Resend, Result};
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     dotenv().ok();
     let api_key = env::var("RESEND_API_KEY").expect("API KEY not found");
-    println!("API KEY: {}", api_key);
+
+    let resend = Resend::new(&api_key);
+
+    let email = vec![
+        CreateEmailBaseOptions::new(
+            "Acme <updates@aswito.com>",
+            vec!["oukojulius224@gmail.com"],
+            "Test Email",
+        )
+        .with_html("<p>This is a test email on how to send multiple emails</p>"),
+    ];
+
+    let email = resend.batch.send(email).await?;
+
+    Ok(())
 }
